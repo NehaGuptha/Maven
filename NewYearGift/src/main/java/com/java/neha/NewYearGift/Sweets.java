26177a9fad28428c5681a889f6ada9ef091fc635
NehaGuptha <neha.kodipaka@gmail.com>
2020-02-09 21:50:22 -0800

    First Commit Up

---
+++ NewYearGift/src/main/java/com/java/neha/NewYearGift/Sweets.java
@@ -0,0 +1,13 @@
+package com.java.neha.NewYearGift;
+
+public abstract class Sweets {
+	protected abstract String getType();
+	  int weightOfSweet,costOfSweet;
+	  String nameOfSweet;
+	  Sweets(String nameOfSweet,int weightOfSweet,int costOfSweet){
+		  this.nameOfSweet = nameOfSweet;
+		  this.weightOfSweet = weightOfSweet;
+		  this.costOfSweet = costOfSweet;
+	  }
+	
+}
